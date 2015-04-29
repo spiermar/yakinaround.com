a8cf58245b9cfeeaf15b4b6a9d5a213a76a95ffe
Martin Spier <spiermar@gmail.com>
2015-04-29 13:10:06 -0700

    Smooth scroll on links.

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,21 @@ $(document).ready(function(){
 		}
 	});
 
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
 	var timerId = countdown(
-		new Date(2015, 6, 19, 8, 0, 0), 
+		new Date(2015, 6, 19, 8, 0, 0),
 		function(ts) {
 			$('#countdown').html(ts.toString());
 		},
